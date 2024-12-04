22d06fa15a1821019196f3abfea7b7d994745c6c
mlewandowicz95 <mateusz.lewandowicz95@gmail.com>
2024-12-04 21:18:36 +0100

    Create simple CRUD to transaction model.

--- BudgetManagerAPI/Controllers/UserController.cs
+++ BudgetManagerAPI/Controllers/UserController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 
 namespace BudgetManagerAPI.Controllers
 {
-    [Authorize]
+  //  [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
---
+++ BudgetManagerAPI/DTO/TransactionDto.cs
@@ -0,0 +1,25 @@
+﻿namespace BudgetManagerAPI.DTO
+{
+    public class TransactionRequestDto
+    {
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; } // "Income" or "Expense", 
+        public int CategoryId { get; set; }
+        public string? Description { get; set; }
+        public bool IsRecurring { get; set; } = false;
+        public DateTime Date { get; set; }
+    }
+
+    public class TransactionResponseDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; } // "Income" or "Expense", 
+        public int CategoryId { get; set; }
+        public string? Description { get; set; }
+        public bool IsRecurring { get; set; } = false;
+        public DateTime Date { get; set; }
+    }
+}
