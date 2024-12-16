fb52ec936e9f4971eea8ef860c5289481b7a2b28
mlewandowicz95 <mateusz.lewandowicz95@gmail.com>
2024-12-16 04:49:54 +0100

    Create a dashboard panel.

---
+++ BudgetManagerAPI/Controllers/DashboardController.cs
@@ -0,0 +1,86 @@
+﻿using BudgetManagerAPI.Data;
+using BudgetManagerAPI.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BudgetManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("dashboard")]
+        public async Task<IActionResult> GetDashboardSummary()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if(string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized(new { Message = "User is not authenticated." });
+            }
+
+            int parsedUsedId = int.Parse(userId);
+
+            // Pobierz transakcje z bieżacego miesiaca
+            var currentDate = DateTime.UtcNow;
+            var transactions = await _context.Transactions
+                .Where(t => t.UserId == parsedUsedId && t.Date.Month == currentDate.Month && t.Date.Year == currentDate.Year)
+                .Include(t => t.Category)
+                .ToListAsync();
+
+            // podsumowanie p[rzytchodów i wydatków
+            var totalIncome = transactions.Where(t => t.Type == Enums.TransactionType.Income).Sum(t => t.Amount);
+            var totalExpenses = transactions.Where(t => t.Type == Enums.TransactionType.Expense).Sum(t => t.Amount);
+
+
+            // Ostatbnie transakcje (maks. 5)
+            var recentTransactions = transactions
+                .OrderByDescending(t => t.Date)
+                .Take(5)
+                .Select(t => new TransactionDto
+                {
+                    Date = t.Date,
+                    Amount = t.Amount,
+                    CategoryName = t.Category.Name,
+                    Description = t.Description,
+                    Type = t.Type.ToString()
+                })
+                .ToList();
+
+            // download all goals
+            var goals = await _context.Goals
+                .Where(g => g.UserId == parsedUsedId)
+                .ToListAsync();
+
+            var goalsDtos = goals.Select(g => new GoalDto
+            {
+                Name = g.Name,
+                TargetAmount = g.TargetAmount,
+                CurrentProgress = g.CurrentProgress,
+                DueDate = g.DueDate
+            }).ToList();
+
+
+            // build dashboard
+            var dashboardSummary = new DashbordSummaryDto
+            {
+                TotalExpenses = totalExpenses,
+                TotalIncome = totalIncome,
+                RecentTransactions = recentTransactions,
+                SavingGoals = goalsDtos
+            };
+
+            return Ok(dashboardSummary);
+        }
+
+    }
+}
---
+++ BudgetManagerAPI/DTO/DashbordUserDto.cs
@@ -0,0 +1,30 @@
+﻿namespace BudgetManagerAPI.DTO
+{
+    public class DashbordSummaryDto
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpenses { get; set; }
+        public decimal Balance => TotalIncome - TotalExpenses;
+
+        public List<TransactionDto> RecentTransactions { get; set; } // last transactions
+        public List<GoalDto> SavingGoals { get; set; } // cele oszczednosciowe
+    }
+
+    public class TransactionDto
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string CategoryName { get; set; }
+        public string? Description { get; set; }
+        public string Type { get; set; } // "Income" or "Expense
+    }
+
+    public class GoalDto
+    {
+        public string Name { get; set; }
+        public decimal TargetAmount { get; set; }
+        public decimal CurrentProgress { get; set; }
+        public decimal ProgressPercentage => (CurrentProgress / TargetAmount) * 100;
+        public DateTime? DueDate { get; set; }
+    }
+}
