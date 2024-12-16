9098bbd49309ddcba1f0caec7af2330b9d80e767
mlewandowicz95 <mateusz.lewandowicz95@gmail.com>
2024-12-16 05:03:59 +0100

    Add resend activation link action and modify parameter in RequestPasswordReset.

---
+++ BudgetManagerAPI/DTO/ResetPasswordRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManagerAPI.DTO
+{
+    public class ResetPasswordRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
